ad893538e99a875be171758792cb0d3cd1f47e7c
Bethany Weddle <35465636+bnweddle@users.noreply.github.com>
2020-05-05 12:02:02 -0500

    Create pthread_test.c
    Very much a rough draft

---
+++ pthread_test.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#define MAX_THREADS 4
+#define BUFFSIZE 12
+//define FILENAME "a.txt"
+#define FILENAME "/homes/dan/625/wiki_dump.txt"
+
+int main()
+{
+    FILE *fp;
+    int count = 0;  // Line counter (result)
+    char c = 0;  // To store a character read from file
+    int sum = 0;
+    int *sums = malloc(BUFFSIZE * sizeof(int)); 
+    int i = 0;
+    int j = 0;
+    
+    int t, rc;
+    pthread_t threads[MAX_THREADS];
+    pthread_attr_t attr;
+    void *status;
+
+    /* Initialize and set thread detached attribute */
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    // Open the file
+    fp = fopen(FILENAME, "r");
+
+    // Check if file exists
+    if (fp == NULL)
+    {
+       printf("Could not open file"); // breaks here
+       return 0;
+    }
+
+    // Extract characters from file and store in character c
+       for(i=0; i <= BUFFSIZE; i++) {
+	  c = getc(fp);
+	  while(c != '\n') {
+            if(c == EOF) {
+              break;
+            }
+	    sum += (int)c;
+	    c = getc(fp);
+          }
+	  printf("i:%d sum:%d\n",i,sum);
+	  sums[i] = sum;
+	  count++;
+	  sum = 0;
+       }
+       
+     for (t = 0; t < MAX_THREADS; t++ ){
+        rc = pthread_create(&threads[t], &attr, FindSums(count), (void *)t);
+        if (rc)
+        {
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+        }
+    }
+
+    /* Free attribute and wait for the other threads */
+    pthread_attr_destroy(&attr);
+    for(t=0; t< MAX_THREADS; t++)
+    {
+      rc = pthread_join(threads[i], &status);
+      if (rc)
+      {
+        printf("ERROR; return code from pthread_join() is %d\n", rc);
+        exit(-1);
+      }
+    }
+   FindSums(count);
+
+      
+   fclose(fp);
+   return 0;
+}
+
+public void FindSums(int count)
+{
+    for(j = 0; j < count; j++) 
+    {
+	      printf("tid:%d line %d-%d: %d\n",omp_get_thread_num(),j,j+1,(sums[j+1]-sums[j]));
+    }
+    
+    pthread_exit(NULL);
+}
